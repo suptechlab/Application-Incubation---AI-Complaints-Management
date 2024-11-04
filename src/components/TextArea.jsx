3790f560e78aa972fa908f00be4b331144986cb7
Mukesh <Mukesh.Mewara@matellio.com>
2024-11-04 18:32:32 +0530

    Initial test commit

--- src/components/TextArea.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const TextArea = (props) => {
-  return (
-    <textarea {...props} />
-)
-};
-
-export default TextArea;
--- src/validations/claimType.validation.js
+++
@@ -1,9 +0,0 @@
-import * as Yup from "yup";
-
-const validationSchema = Yup.object({
-    name: Yup.string().max(250, "Claim type name cannot exceed 250 characters").required("Name of claim type is required."),
-    description: Yup.string().max(512, "Description cannot exceed 512 characters").required("Description is required.")
-});
-
-
-export { validationSchema };
