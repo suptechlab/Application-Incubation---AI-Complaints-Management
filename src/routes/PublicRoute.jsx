4b05959636330fe1328b4306dbd4b250e039d49f
Tarun <tarun.sen@matellio.com>
2024-11-21 16:48:52 +0530

    public console try

--- src/routes/PublicRoute.jsx
+++ src/routes/PublicRoute.jsx
@@ -5,19 +5,23 @@ import { AuthenticationContext } from "../contexts/authentication.context";
 
 const PublicRoute = ({ element }) => {
     const { isAuthenticated } = useContext(AuthenticationContext);
-
+    console.log('isAuthenticated',isAuthenticated)
+    console.log('isAuthenticated',element)
     if (isAuthenticated) {
+       
         // Check if the user is trying to access the login, forgot password, or reset password page
         const allowedPages = ["Login", "ForgotPassword", "ResetPassword" ];
 
         if (element && element.type && allowedPages.includes(element.type.name)) {
+            console.log(' true includes 16 ',element.type)
             return <Navigate to='/dashboard' replace />;
         }
+        console.log(' false 19 ')
         // else{
         //     return <Navigate to='/login' replace />;
         // }
     }
-
+    console.log('Not Authenticated 24 ',element)
     return element;
 };
 
