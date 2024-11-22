4efeb260d86fe6f3d67dc882fa89ae0e93de4fd3
Mukesh <Mukesh.Mewara@matellio.com>
2024-11-22 20:04:25 +0530

    SPC-499: Sprint 3 - #Feat Front My account page UI #WIP

--- src/components/common/ReactTable.jsx
+++ src/components/common/ReactTable.jsx
@@ -74,7 +74,7 @@ export default function ReactTable({
     <div className="d-flex flex-column h-100 small table-cover-main">
       {/* <Loader isLoading={isLoading} /> */}
       <BTable striped bordered hover responsive className="mb-0">
-        <thead className="fs-15">
+        <thead>
           {table.getHeaderGroups().map((headerGroup) => (
             <tr key={headerGroup.id}>
               {headerGroup.headers.map((header) => {
@@ -87,7 +87,7 @@ export default function ReactTable({
                     {header.column.getCanSort() ? (
                       <Button
                         variant="link"
-                        className="align-items-center border-0 cursor-pointer d-flex fs-15 fw-semibold gap-2 link-dark p-0 table-sorting text-decoration-none user-select-none w-100 text-start"
+                        className="align-items-center border-0 cursor-pointer d-flex fw-semibold gap-2 link-dark p-0 table-sorting text-decoration-none user-select-none w-100 text-start"
                         onClick={header.column.getToggleSortingHandler()}
                         title={
                           header.column.getCanSort()
