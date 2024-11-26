ed73834cc6831f9fb3aea3eecb0ab7ff2f5c96e1
Pawan <pawan.kumar@matellio.com>
2024-11-26 23:53:33 +0530

    Added new enum for user master apis

---
+++ src/main/java/com/seps/user/enums/ClaimTicketPriorityEnum.java
@@ -0,0 +1,20 @@
+package com.seps.user.enums;
+
+import com.seps.user.component.EnumWithDescription;
+
+public enum ClaimTicketPriorityEnum implements EnumWithDescription {
+    LOW("enum.low"),
+    MEDIUM("enum.medium"),
+    HIGH("enum.high");
+
+    private final String key;
+
+    ClaimTicketPriorityEnum(String key) {
+        this.key = key;
+    }
+
+    @Override
+    public String getDescription() {
+        return this.key;
+    }
+}
