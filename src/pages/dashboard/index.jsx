74d37bb2a822ad757ea44876e96270a6caf229e8
Mukesh <Mukesh.Mewara@matellio.com>
2024-12-02 19:23:37 +0530

    SPC-585: Sprint 4 - #Feat Admin Dashboard Page Total Claims UI Done #WIP

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,55 +1,12 @@
 import React, { useState } from "react";
-import { Card, Stack } from "react-bootstrap";
-import { useTranslation } from "react-i18next";
+import { Stack } from "react-bootstrap";
 import CommonDatePicker from "../../components/commonDatePicker";
 import Loader from "../../components/Loader";
-import ReactSelect from "../../components/ReactSelect";
-import InfoCards from "../../components/infoCards";
-import { MdConfirmationNumber, MdDoDisturb, MdHourglassEmpty, MdPending, MdTaskAlt } from "react-icons/md";
+import TotalClaimsSection from "./sections/total-claims";
 
 export default function Dashboard() {
   const [startDate, setStartDate] = useState();
-  const { t } = useTranslation();
   let loading = false;
-
-  // Info Cards Data
-  const cardsData = [
-    {
-      bgColor: 'bg-primary',
-      Icon: <MdConfirmationNumber size={24} />,
-      title: 'New Tickets',
-      value: 5,
-      colProps: { sm: 6, md: 4, className: "col-xl" }
-    },
-    {
-      bgColor: 'bg-orange',
-      Icon: <MdHourglassEmpty size={24} />,
-      title: 'Tickets in Progress',
-      value: 2,
-      colProps: { sm: 6, md: 4, className: "col-xl" }
-    },
-    {
-      bgColor: 'bg-custom-orange',
-      Icon: <MdPending size={24} />,
-      title: 'Pending Tickets',
-      value: 1,
-      colProps: { sm: 6, md: 4, className: "col-xl" }
-    },
-    {
-      bgColor: 'bg-custom-green',
-      Icon: <MdTaskAlt size={24} />,
-      title: 'Closed Tickets',
-      value: 2,
-      colProps: { sm: 6, md: 4, className: "col-xl" }
-    },
-    {
-      bgColor: 'bg-danger',
-      Icon: <MdDoDisturb size={24} />,
-      title: 'Rejected Tickets',
-      value: 2,
-      colProps: { sm: 6, md: 4, className: "col-xl" }
-    },
-  ];
 
   return (
     <React.Fragment>
@@ -80,51 +37,8 @@ export default function Dashboard() {
           </Stack>
         </div>
 
-        <Card className="border-0 shadow">
-          <Card.Header className="bg-body">
-            <Stack
-              direction="horizontal"
-              gap={2}
-              className="flex-wrap my-1"
-            >
-              <Stack
-                direction="horizontal"
-                gap={2}
-                className="flex-wrap me-auto"
-              >
-                <div className="fw-semibold fs-4 mb-0">
-                  Total Claims <span className="fs-14 fw-normal">(submitted across all FIs)</span>
-                </div>
-                <div className="bg-primary bg-opacity-10 p-2 small rounded"><span className="me-2">Average Resolution Time:</span> <span className="fw-semibold">1.2 Days</span></div>
-              </Stack>
+        <TotalClaimsSection />
 
-              <div className="custom-min-width-160 flex-grow-1 flex-md-grow-0">
-                <ReactSelect
-                  wrapperClassName="mb-0"
-                  class="form-select "
-                  placeholder="Select"
-                  id="fisAndSeps"
-                  size="sm"
-                  options={[
-                    {
-                      label: "FIs & SEPS",
-                      value: "",
-                    },
-                    {
-                      label: "Option 1",
-                      value: 'option-1',
-                    },
-                  ]}
-                />
-              </div>
-            </Stack>
-          </Card.Header>
-          <Card.Body>
-            <div className="info-cards mb-3">
-              <InfoCards cardsData={cardsData} rowClassName="g-3 text-nowrap" />
-            </div>
-          </Card.Body>
-        </Card>
       </div>
     </React.Fragment>
   );
